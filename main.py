bd6be960a365249fb5074272759dee11566f2d35
Sanchez0823 <arturosanchezavocado23@gmail.com>
2025-10-17 21:32:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, json, csv, """jpeg,""" os
+import sys, json, csv, png, os
 from dataclasses import dataclass, asdict
 from typing import List, Dict
 
@@ -13,26 +13,58 @@ from PySide6.QtWidgets import (
 )
 
 # App title and files
-APP_TITLE = "My Art Collections"
-DATA_FILE = "art_collec.json"
+APP_TITLE = "My Art Collection"
+DATA_FILE = "art_collections.json"
 
 """ SETS """
 @dataclass
-class Comics:
+class Comic:
   title: str
   year: int
-  """ Comming soon:
-  medium: str #paper, pen/pencil/marker
-  artist: str
-  """
+  num_panels: int
   
 @dataclass
 class Doodle:
    title: str
    year: int
-   """ Comming soon:
    medium: str #paper, pen/pencil/marker
-   artist: str
-   """
+
+class Painting:
+    title: str
+    year: int
+    area: int
+
 # Comic Pictures
-class ComicPics(QDialog)
+class ComicDialogue(QDialog):
+    def __init__(self, parent=None, comic: Comic):
+        super().__init__(parent)
+        self.setWindowTitle("My Art Collection")
+        self.setMinimumWidth(400)
+        self.title = QLineEdit()
+        self.year = QLineEdit()
+        self.num_panels = QLineEdit()
+        if comic:
+            self.title.setText(comic.title)
+            self.year.setText(comic.year)
+            self.num_panels.setText(comic.num_panels)
+
+        form = QFormLayout()
+        form.addRow("Title*", self.title)
+        form.addRow("Year*", self.year)
+        form.addRow("Num Panels", self.num_panels)
+
+        buttons = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        buttons.accepted.connect(self.accept)
+        buttons.rejected.connect(self.reject)
+
+        lay = QVBoxLayout(self)
+        lay.addLayout(form)
+        lay.addWidget(buttons)
+
+    def get_details(self) -> dict:
+        return {"title": self.title.text().strip(),
+                "year": self.year.text().strip(),
+                "num_panels": self.num_panels.text().strip(),}
+
+    # class DoodleDialog(QDialog):
+
